05969fe27d6ef8dbfcce42350042c33786d88f2c
KaushalSahu <kaushal.raj.sahu@gmail.com>
2019-04-15 22:51:48 +0530

    --allow-empty-message

---
+++ Div 2/Round #551/551A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, t, i, j, min = INT_MAX, newmin, s, d, pos=0;
+	cin >> n >> t;
+
+	for(i = 1; i <= n; i++){
+
+		cin >> s >> d;
+
+		for(j = s; j < t; j+=d);
+		newmin = abs(t - j);
+
+		if(newmin < min){
+		min = newmin;
+		pos = i;
+		}
+	}
+	cout << pos;
+}
